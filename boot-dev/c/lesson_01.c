fd51bfe4d46cff9bb1cafa534665b247c0d1e1b1
Josiel Sousa <josiel.psousa@gmail.com>
2025-02-05 19:04:53 -0300

    chore: lesson 2 compiler error when function does not exists

--- boot-dev/c/lesson_01.c
+++ boot-dev/c/lesson_01.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// cc lesson_01.c -o lesson_01.o && ./lesson_01.o
+// cc boot-dev/c/lesson_01.c -o out/lesson_01.o && ./out/lesson_01.o
 int main() {
   printf("Program in C\n");
   return 0;

7c2e07baf6d5733c5e4906bb08069f2d16c63a58
Josiel Sousa <josiel.psousa@gmail.com>
2025-02-05 19:20:57 -0300

    chore: lesson 03 basic types

---
+++ boot-dev/c/lesson_03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// cc boot-dev/c/lesson_03.c -o out/lesson_03.o && ./out/lesson_03.o
+int main() {
+    int max_recursive_calls = 2;
+    char io_mode = 'w';
+    float throtle_speed = 0.2;
+
+    printf("Max recursive calls: %d\n", max_recursive_calls);
+    printf("IO mode: %c\n", io_mode);
+    printf("Throtle speed: %f\n", throtle_speed);
+}

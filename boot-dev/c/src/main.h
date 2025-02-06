cc2b507b703385fcf0c6db37e3bb58a960883c98
Josiel Sousa <josiel.psousa@gmail.com>
2025-02-06 20:22:57 -0300

    chore: memory layout

--- boot-dev/c/src/main.h
+++ boot-dev/c/src/main.h
@@ -2,8 +2,6 @@
 
 #pragma once
 
-char *get_language();
-
 struct Coordinate {
   int x;
   int y;
@@ -15,6 +13,45 @@ typedef struct Human {
   int age;
   int is_alive;
 } human_t;
+
+// Memory layout
+// poorly_aligned_t - nesse exemplo a struct é mal alinhada
+// char a; -> 1 byte
+// double b; -> 8 bytes
+// char c; -> 1 byte
+// char d; -> 1 byte
+// long e; -> 8 bytes
+// char f; -> 1 byte
+// nesse ela vai ser alocada em um espaço maior, pois o compilador vai adicionar
+// bytes de padding para alinhar os tipos de dados.
+typedef struct {
+  char a;
+  double b;
+  char c;
+  char d;
+  long e;
+  char f;
+} poorly_aligned_t;
+
+// better_aligned_t - nesse exemplo a struct é bem alinhada
+// double b; -> 8 bytes
+// long e; -> 8 bytes
+// char a; -> 1 byte
+// char c; -> 1 byte
+// char d; -> 1 byte
+// char f; -> 1 byte
+// nesse caso a struct vai ser alocada em um espaço menor, pois o compilador
+// não vai adicionar bytes de padding para alinhar os tipos de dados.
+typedef struct {
+  double b;
+  long e;
+  char a;
+  char c;
+  char d;
+  char f;
+} better_aligned_t;
+
+char *get_language();
 
 struct Coordinate new_coordinate(int x, int y, int z);
 
